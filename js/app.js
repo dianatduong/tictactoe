ba0c068bc45d9926c31af0fb612a170e94c4e179
Diana Duong <dduong013@gmail.com>
2016-04-27 18:38:09 -0500

    added reset game

--- js/app.js
+++ js/app.js
@@ -2,28 +2,22 @@ $(function() {
   var player = 1;
   var count = 0;
   $('.square').click(function() {
-  	count = 0;
+    if (count == 9) {
+      $('.square').empty()
+      count = 0;
+      player = 1;
+    }
+    count++
     if (player == 1) {
-      $(this).append('<div><i class="fa fa-times" aria-hidden="true"></i></div>')
+      $(this).append('<div class="clicked"><i class="fa fa-times" aria-hidden="true"></i></div>')
       player = 0;
-      count++
     } else {
       $(this).append('<div><i class="fa fa-circle-o" aria-hidden="true"></i></div>')
       player = 1;
-      count++;
     }
-    endGame()
   })
   $('#reset-btn').click(function() {
     $('.square').empty()
-    $('.square').bind('click');
-    player = 1;
-   	count = 0;
+    count = 0;
   })
-
-  function endGame() {
-    if (count == 9) {
-      $('.square').unbind('click')
-    }
-  }
 })
