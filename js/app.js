00e08e287863bf917dcb62c830594d22b67e1373
Diana Duong <dduong013@gmail.com>
2016-04-27 16:57:21 -0500

    added unbind click

--- js/app.js
+++ js/app.js
@@ -1,20 +1,29 @@
 $(function() {
+  var player = 1;
+  var count = 0;
+  $('.square').click(function() {
+  	count = 0;
+    if (player == 1) {
+      $(this).append('<div><i class="fa fa-times" aria-hidden="true"></i></div>')
+      player = 0;
+      count++
+    } else {
+      $(this).append('<div><i class="fa fa-circle-o" aria-hidden="true"></i></div>')
+      player = 1;
+      count++;
+    }
+    endGame()
+  })
+  $('#reset-btn').click(function() {
+    $('.square').empty()
+    $('.square').bind('click');
+    player = 1;
+   	count = 0;
+  })
 
-var player = 1;
-
-	$('.square').click(function(){
-		if ( player == 1 ){
-			$(this).append('<div><i class="fa fa-times" aria-hidden="true"></i></div>')
-			player = 0;
-		} 
-		else {
-			$(this).append('<div><i class="fa fa-circle-o" aria-hidden="true"></i></div>')
-			player = 1;
-		} 
-	})
-
-	$('#reset-btn').click(function(){
-		$('.square').empty()
-		player = 1;
-	})
+  function endGame() {
+    if (count == 9) {
+      $('.square').unbind('click')
+    }
+  }
 })
