93502467fb2296d535373ab51332360e5bc73c0c
Diana Duong <dduong013@gmail.com>
2016-04-28 15:22:47 -0500

    removed count. fixed additional clicks on divs

--- js/app.js
+++ js/app.js
@@ -1,23 +1,26 @@
 $(function() {
   var player = 1;
-  var count = 0;
+  var playerTwo = $('.playerTwo')
   $('.square').click(function() {
-    if (count == 9) {
-      $('.square').empty()
-      count = 0;
-      player = 1;
-    }
-    count++
     if (player == 1) {
-      $(this).append('<div class="clicked"><i class="fa fa-times" aria-hidden="true"></i></div>')
+      $(this).addClass('playerOne')
+      if (playerTwo) {
+        $('.playerTwo').removeClass('playerOne')
+      }
+    }
+    if ($(this).hasClass('playerOne')) {
+      $(this).html('<div><i class="fa fa-times" aria-hidden="true"></i></div>')
       player = 0;
     } else {
-      $(this).append('<div><i class="fa fa-circle-o" aria-hidden="true"></i></div>')
+      $(this).addClass('playerTwo')
+      $(this).html('<div><i class="fa fa-circle-o" aria-hidden="true"></i></div>')
       player = 1;
     }
   })
   $('#reset-btn').click(function() {
     $('.square').empty()
-    count = 0;
+    $('.square').removeClass('playerOne')
+    $('.square').removeClass('playerTwo')
+    player = 1;
   })
 })
