1e6afaea5ff7d8f63acb595319ede07c866eabf5
Diana Duong <dduong013@gmail.com>
2016-05-24 16:52:13 -0500

    installed Babel

--- build/tictactoe.1.0.0.js
+++ build/tictactoe.1.0.0.js
@@ -44,35 +44,37 @@
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
+	'use strict';
+	
 	// require jQuery
 	var $ = __webpack_require__(1);
 	
-	$(document).ready(function() {
+	$(document).ready(function () {
 	  var player = 1;
-	  var playerTwo = $('.playerTwo')
-	  $('.square').click(function() {
+	  var playerTwo = $('.playerTwo');
+	  $('.square').click(function () {
 	    if (player == 1) {
-	      $(this).addClass('playerOne')
+	      $(this).addClass('playerOne');
 	      if (playerTwo) {
-	        $('.playerTwo').removeClass('playerOne')
+	        $('.playerTwo').removeClass('playerOne');
 	      }
 	    }
 	    if ($(this).hasClass('playerOne')) {
-	      $(this).html('<div><i class="fa fa-times" aria-hidden="true"></i></div>')
+	      $(this).html('<div><i class="fa fa-times" aria-hidden="true"></i></div>');
 	      player = 0;
 	    } else {
-	      $(this).addClass('playerTwo')
-	      $(this).html('<div><i class="fa fa-circle-o" aria-hidden="true"></i></div>')
+	      $(this).addClass('playerTwo');
+	      $(this).html('<div><i class="fa fa-circle-o" aria-hidden="true"></i></div>');
 	      player = 1;
 	    }
-	  })
-	  $('#reset-btn').click(function() {
-	    $('.square').empty()
-	    $('.square').removeClass('playerOne')
-	    $('.square').removeClass('playerTwo')
+	  });
+	  $('#reset-btn').click(function () {
+	    $('.square').empty();
+	    $('.square').removeClass('playerOne');
+	    $('.square').removeClass('playerTwo');
 	    player = 1;
-	  })
-	})
+	  });
+	});
 
 /***/ },
 /* 1 */
